bc4b47b1a1a2908c7aeeba575ce1438c56f6a034
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-13 21:59:03 +0530

    Use Markdown

--- plugins/start.py
+++ plugins/start.py
@@ -9,4 +9,4 @@ from config import Config
 
 @Client.on_message(Filters.command(["start"]))
 async def start(bot, msg):
-  await bot.send_message(msg.chat.id, f"Hi {msg.from_user.id}. I am Attach Bot. I can attach medias to your long text.", reply_to_message_id=msg.message_id)
+  await bot.send_message(msg.chat.id, f"Hi [{msg.from_user.first_name]({msg.from_user.id}). I am Attach Bot. I can attach medias to your long text.", parse_mode="markdown", reply_to_message_id=msg.message_id)
