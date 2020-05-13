9ab58746b02271eefbe3dcb5818e7200260aca58
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-13 21:18:07 +0530

    Initial commit

--- plugins/attach.py
+++ plugins/attach.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Anandpskerala
 
+import pyrogram
+
+from pyrogram import Client, Filters
+
+#configs for the bot
+from config import Config
+
+@Client.on_message(Filters.text)
+async def attach(bot, msg):
+  if msg.reply_to_message == None:
+    await msg.reply("Reply to a media to get an attached Media")
+  else:
+    m = await bot.forward_messages(Config.CHANNEL_USERNAME, msg.chat.id, msg.reply_to_message.message_id)
+    m_id = m.message_id
+    link = f"https://t.me/{Config.CHANNEL_USERNAME}/{m_id}"
+    await bot.send_message(msg.chat.id, msg.text + "[ ](f'{link}')", parse_mode="markdown", disable_web_page_preview=False)
