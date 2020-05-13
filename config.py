50785d8b233f1cb1ebe0faeefb7969c8617bb4d4
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-13 20:53:09 +0530

    Added channel

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ class Config(object):
   APP_ID = int(os.environ.get("API_ID", 12345))
   API_HASH = os.environ.get("API_HASH")
   DOWNLOAD_LOCATION = "./DOWNLOADS"
+  #CHANNEL_USERNAME without '@'
+  CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "")
