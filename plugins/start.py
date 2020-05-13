84c773e74eb5c67241aef5642f76b421cfcceaf6
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-13 21:01:31 +0530

    Initial commit

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, Filters
 
+#Secret configs
+from config import Config
+
+@Client.on_message(Filters.command(["start"]))
+async def start(bot, msg):
+  await bot.send_message(msg.chat.id, f"Hi {msg.from_user.id}. I am Attach Bot. I can attach medias to your long text.", reply_to_message_id=msg.message_id)
