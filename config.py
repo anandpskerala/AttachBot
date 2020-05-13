76c4aba394697a075796cb55238f6f85342e3751
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-13 20:47:40 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Anandpskerala
 
+import os
+
+class Config(object):
+  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+  APP_ID = int(os.environ.get("API_ID", 12345))
+  API_HASH = os.environ.get("API_HASH")
+  DOWNLOAD_LOCATION = "./DOWNLOADS"
