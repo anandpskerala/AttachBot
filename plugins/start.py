275e600a24895ed30f2fe3bc2df2bfa75f868e68
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-13 22:07:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,4 +9,4 @@ from config import Config
 
 @Client.on_message(Filters.command(["start"]))
 async def start(bot, msg):
-  await bot.send_message(msg.chat.id, f"Hi <a href={msg.from_user.id}>{msg.from_user.first_name}</a>. I am Attach Bot. I can attach medias to your long text.", parse_mode="html", reply_to_message_id=msg.message_id)
+  await bot.send_message(msg.chat.id, f"Hi <a href='{msg.from_user.id}'>{msg.from_user.first_name}</a>. I am Attach Bot. I can attach medias to your long text.", parse_mode="html", reply_to_message_id=msg.message_id)
