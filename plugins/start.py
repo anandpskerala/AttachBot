4445f8886c77f72b0dd930416ac5baa457f4dde9
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-14 23:35:49 +0530

    Use Markdown

--- plugins/start.py
+++ plugins/start.py
@@ -9,4 +9,4 @@ from config import Config
 from telegram import ParseMode
 
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi <a href='tg://user?id={update.message.from_user.id}'>{update.message.from_user.first_name}</a>. I am Attach Bot. I can attach medias to your long text.", parse_mode=ParseMode.HTML)
+  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi [{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) I am Attach Bot. I can attach medias to your long text.", parse_mode=ParseMode.MARKDOWN)
