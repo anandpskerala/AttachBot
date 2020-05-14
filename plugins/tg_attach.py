3d534b674e5deba42ffc9dd7c61e143b7f3afd4f
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-14 23:05:45 +0530

    Use python-telegram-bot

--- plugins/tg_attach.py
+++ plugins/tg_attach.py
@@ -17,7 +17,7 @@ def attach(update, context):
     update.message.reply_text("Reply to a media to get an attached Media")
   else:
     m = context.bot.forward_message("@" + Config.CHANNEL_USERNAME, update.effective_chat.id, update.message.reply_to_message.message_id)
-    m_id = m.message.message_id
+    m_id = m.effective_message.message_id
     link = "https://t.me/{}/{}".format(Config.CHANNEL_USERNAME, m_id)
     print(link)
     context.bot.send_message(update.effective_chat.id, update.message.text + "<a href='{}'>{}</a>".format(link, " "), parse_mode=ParseMode.HTML)
