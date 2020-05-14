f08b4bfc04d9a1279559735ed35090f7487544e7
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-14 22:19:37 +0530

    Use python-telegram-bot

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ import os
 
 class Config(object):
   TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-  APP_ID = int(os.environ.get("API_ID", 12345))
-  API_HASH = os.environ.get("API_HASH", "")
-  DOWNLOAD_LOCATION = "./DOWNLOADS"
   #CHANNEL_USERNAME without '@'
   CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "")
+  CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 12345))
