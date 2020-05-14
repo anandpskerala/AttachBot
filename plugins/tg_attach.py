c0f56564b187a998f016f36a260c34f29926cffc
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-05-14 22:44:42 +0530

    Use python-telegram-bot

--- plugins/tg_attach.py
+++ plugins/tg_attach.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 # (c) Anandpskerala
 
-import pyrogram
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-from pyrogram import Client, Filters
 
 #configs for the bot
 from config import Config
 
-@Client.on_message(Filters.text)
-async def attach(bot, msg):
-  if msg.reply_to_message == None:
-    await msg.reply("Reply to a media to get an attached Media")
+def attach(update, context):
+  if update.message.reply_to_message == None:
+    update.message.reply_text("Reply to a media to get an attached Media")
   else:
-    m = await bot.forward_messages(Config.CHANNEL_USERNAME, msg.chat.id, msg.reply_to_message.message_id)
-    m_id = m.message_id
+    m = context.bot.forward_messages("@" + Config.CHANNEL_USERNAME, update.effective_chat.id, update.reply_to_message.message_id)
+    m_id = m.message.message_id
     link = "https://t.me/{}/{}".format(Config.CHANNEL_USERNAME, m_id)
     print(link)
-    await bot.send_message(msg.chat.id, msg.text + "<a href='{}'>{}</a>".format(link, "."), parse_mode="html", disable_web_page_preview=False)
+    context.bot.send_message(update.effective_chat.id, update.message.text + "<a href='{}'>{}</a>".format(link, " "), parse_mode=ParseMode.HTML)
