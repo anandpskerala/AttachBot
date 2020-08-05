b53ae2caa63992c5120f921c17bda528bfdca1a9
[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-08-05 15:51:35 +0530

    Fixed the broken markdown

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,6 @@
 #Secret configs
 from config import Config
 
-from telegram import ParseMode
 
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi [{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) I am Attach Bot. I can attach medias to your long text.", parse_mode=ParseMode.MARKDOWN)
+  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi [{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) I am Attach Bot. I can attach medias to your long text.", parse_mode="Markdown")
